1234d72b20e4e9e9c9b1a1b979374d008ef5692b
Rohit Patel <115739317+rohitpatel73@users.noreply.github.com>
2023-11-02 00:59:24 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,55 @@
+var timer = 20;
+var score = 0;
+var hitrn = 0;
+
+
+function increaseScore() {
+    document.querySelector("#scoreval").textContent = score;
+    score += 10;
+}
+
+function getnewwhit() {
+    hitrn = Math.floor(Math.random() * 10);
+    document.querySelector("#hitval").textContent = hitrn;
+}
+
+function makebubble() {
+    var clutter = "";
+
+    for (var i = 1; i < 200; i++) {
+        var rn = Math.floor(Math.random() * 10)
+        clutter += `<div class="bubble">${rn}</div>`
+    }
+
+    document.querySelector("#pbtm").innerHTML = clutter
+}
+
+function runTimer() {
+    var timerint = setInterval(function () {
+
+        if (timer > 0) {
+            timer--;
+            document.querySelector("#timerval").textContent = timer;
+        }
+        else {
+            clearInterval(timerint);
+            document.querySelector("#pbtm").innerHTML = `<h1>Game Over !</h1>`;
+        }
+    }, 1000);
+}
+
+document.querySelector("#pbtm").addEventListener("click", function (dets) {
+    var clickednum = Number(dets.target.textContent);
+    if (clickednum == hitrn) {
+        increaseScore();
+        getnewwhit();
+        makebubble();
+    }
+})
+
+
+
+getnewwhit();
+runTimer();
+makebubble();
+increaseScore();
